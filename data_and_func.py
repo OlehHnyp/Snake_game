9d35ad20623c3d1a254a6988c1f88dded0af26b9
Oleh Hnyp <lefk6230@gmail.com>
2020-12-29 10:04:37 +0200

    v1

--- data_and_func.py
+++ data_and_func.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 WHITE = (255, 255, 255)
+VIOLET = (138,43,226)
 YELLOW = (255, 255, 102)
 BLUE = (102, 178, 255)
 SUPER_BLUE = (0, 0, 255)
@@ -11,23 +13,47 @@ INTERVAL = 2
 
 pygame.init()
 
+
 clock = pygame.time.Clock()
 caption = "The SNAKE game"
 screen_width = pygame.display.Info().current_w -100
 screen_height = pygame.display.Info().current_h -100
 screen_size = [screen_width, screen_height]
+screen = pygame.display.set_mode(screen_size)
 free_x = screen_width % (BLOCK_SIZE+INTERVAL)
 free_y = screen_height % (BLOCK_SIZE+INTERVAL)
 header_margin = 3*(BLOCK_SIZE+INTERVAL)
 question_margin = header_margin + free_y
 block_columns = screen_width // (BLOCK_SIZE+INTERVAL)
 block_rows = (screen_height-header_margin) // (BLOCK_SIZE+INTERVAL)
-
-print(screen_width)
-
+answer_courier =  pygame.font.SysFont('courier', 20)
 
 
-def add_block(screen, color, column, row):
+print(print(block_rows))
+
+def add_answers(column, row, answer):
+    x = free_x/2 +(BLOCK_SIZE+INTERVAL) * column
+    y = (BLOCK_SIZE+INTERVAL) * row + header_margin + free_y
+
+    pygame.draw.rect(screen, VIOLET, [x, 
+                                      y,
+                                      BLOCK_SIZE*2 + INTERVAL,
+                                      BLOCK_SIZE*2 + INTERVAL]
+                      )
+    interval1 = 20
+    interval2 = 35
+    if len(answer)==2:
+        interval1 = 5
+        text1 = answer_courier.render(answer[1], 0, WHITE)
+        screen.blit(text1,(x, y + interval2))
+        print('1')
+
+    text = answer_courier.render(answer[0], 0, WHITE)
+    screen.blit(text,(x, y + interval1))
+                             
+
+
+def add_block(color, column, row):
     pygame.draw.rect(screen, color,
                      [free_x/2 +(BLOCK_SIZE+INTERVAL) * column,
                       (BLOCK_SIZE+INTERVAL) * row + header_margin + free_y,
@@ -36,19 +62,45 @@ def add_block(screen, color, column, row):
                       )
 
 
-def draw_margins(screen, color):
+def draw_margins(color):
     pygame.draw.rect(screen, color, (0, 0, screen_width, question_margin))
     pygame.draw.rect(screen, color, (0, question_margin, free_x/2, screen_height-question_margin))
     pygame.draw.rect(screen, color, (screen_width - free_x/2, question_margin, free_x/2, screen_height-question_margin))
 
 class Blocks():
 
-    def __init__(self, column, row, n=1):
+    def __init__(self, column, row):
         self.c = column
         self.r = row
-        self.n = n
 
-snake = [Blocks(8, 9), Blocks(9, 9)]
-move_x = 1
-move_y = 0
+    def isinside(self):
+        return -1<self.c<block_columns and -1<self.r<block_rows
+    
+    def __eq__(self, other):
+        return self.c==other.c and self.r==other.r
+
+class AnswerBlocks(Blocks):
+    def __init__(self, column, row, answer,):
+        self.c = column
+        self.r = row
+        self.answer = answer
+
+
+    def snake_in_answer(self,snake):
+        return -1<(snake.c-self.c)<2 and -1<(snake.r-self.r)<2
+                    
+
+
+snake = [Blocks(0, 9), Blocks(0, 8), Blocks(0, 7), Blocks(0, 6), Blocks(0, 5)]
+move_x = reserve_move_x = 1
+move_y = reserve_move_y = 0
+
+question1 = "Eat all boolean values"
+
+answers1 = [AnswerBlocks(2, 3, ['True']),
+            AnswerBlocks(10,5, ['False']),
+            # AnswerBlocks(15, 15, ['Lie']),
+            AnswerBlocks(4, 5, ['Yes']),
+            AnswerBlocks(15,10, ['Lie'])
+            ]
 
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pygame
+
 import data_and_func as d
-
-
 
 pygame.init()
 
 screen = pygame.display.set_mode(d.screen_size)
 pygame.display.set_caption(d.caption)
-
 
 
 done = False
@@ -16,28 +14,63 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and d.move_y != 1:
+                d.reserve_move_x = 0
+                d.reserve_move_y = -1
+            elif event.key == pygame.K_DOWN and d.move_y != -1:
+                d.reserve_move_x = 0
+                d.reserve_move_y = 1
+            elif event.key == pygame.K_LEFT and d.move_x != 1:
+                d.reserve_move_x = -1
+                d.reserve_move_y = 0
+            elif event.key == pygame.K_RIGHT and d.move_x != -1:
+                d.reserve_move_x = 1
+                d.reserve_move_y = 0
+    
+    
+
+
 
     screen.fill(d.WHITE)
-    d.draw_margins(screen, d.SUPER_BLUE)
+    d.draw_margins(d.SUPER_BLUE)
+    snake_head = d.snake[-1]
+    
+    
 
     for row in range(d.block_rows):
         for column in range(d.block_columns):
             color = d.BLUE
             if (row+column)%2 == 0:
                 color = d.YELLOW
-            d.add_block(screen, color, column, row)
-    
+            d.add_block(color, column, row)
     for block in d.snake:
-        d.add_block(screen, d.RED, block.c, block.r)
+        d.add_block(d.RED, block.c, block.r)
+
+
+    for el in d.answers1:
+        d.add_answers(el.c, el.r, el.answer)
+
+    d.move_x = d.reserve_move_x
+    d.move_y = d.reserve_move_y
     
-    snake_head = d.snake[-1]
     new_head = d.Blocks(snake_head.c + d.move_x, snake_head.r + d.move_y)
+    if not new_head.isinside():
+        done = True
+    
+    if new_head in d.snake:
+        done = True
+    
+    for i, el in enumerate(d.answers1):
+        if el.snake_in_answer(new_head):
+            d.answers1.pop(i)
+
     d.snake.append(new_head)
     d.snake.pop(0)
-    print(d.snake)
+    
     
     pygame.display.flip()
-    d.clock.tick(2)
+    d.clock.tick(3)
 
 
     
